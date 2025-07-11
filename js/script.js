537ebb1f55ac1c94c2ff34f1820413c5bec03b96
Dinesh Sheran <dineshsheran@gmail.com>
2025-07-11 14:01:09 +0900

    latest

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.warn("scrollBtn or nextSection not found in the DOM.");
   }
 
+  // Swiper initialization
   const swiper = new Swiper(".swiper", {
     slidesPerView: 3,
     spaceBetween: 50,
@@ -26,5 +27,19 @@ document.addEventListener("DOMContentLoaded", () => {
         slidesPerView: 2,
       },
     },
+  });
+
+  // FAQ Answer Toggle
+  const faqButtons = document.querySelectorAll(".qestion__btn");
+  faqButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // Find the closest .questions__item and then its answer
+      const item = btn.closest(".questions__item");
+      const answer = item.querySelector(".questions__item__answer");
+      if (answer) {
+        answer.style.display = "block";
+        btn.style.display = "none";
+      }
+    });
   });
 });
