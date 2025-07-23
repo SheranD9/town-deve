43f2ae36f33f39fb984a03e63a7f28a63667c840
Dinesh Sheran <dineshsheran@gmail.com>
2025-07-23 12:56:51 +0900

    index page

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Swiper initialization
   const swiper = new Swiper(".swiper", {
     slidesPerView: 3,
-    spaceBetween: 50,
+    spaceBetween: 20,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -52,4 +52,42 @@ if (footer) {
   } else {
     footer.style.backgroundColor = "#fa4b06"; // Default/home page color
   }
+}
+
+const header = document.querySelector(".header");
+if (header) {
+  if (window.location.pathname.includes("index.html")) {
+    header.style.backgroundColor = "transparent"; // index page color
+  } else {
+    footer.style.backgroundColor = "#52b3e6"; // Default color
+  }
+}
+
+//wether
+document.addEventListener("DOMContentLoaded", () => {
+  const API_KEY = "145fc348fce54cfc84433152250906";
+  const location = "nagoya";
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&lang=ja`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const temp = data.current.temp_c;
+      const icon = "./images/cloudy.png";
+      // const icon = data.current.condition.icon;
+      const condition = data.current.condition.text;
+
+      // Update HTML
+      document.querySelector(".weather__icon img").src = icon;
+      document.querySelector(".weather__icon img").alt = condition;
+      document.querySelector(".weather__icon span").textContent = `${temp}°C`;
+    })
+    .catch((error) => {
+      console.error("Failed to fetch weather data:", error);
+    });
+});
+
+const cards = document.querySelectorAll(".sectionOne__card");
+if (cards.length >= 3) {
+  cards[2].style.border = "none"; // 3rd card (index 2)
 }
