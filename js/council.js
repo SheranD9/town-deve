f9cbd19995e50e26466dd6a7fe36f8704a32c3c7
kanakanakan <kamimizu.kanade@gmail.com>
2025-07-25 16:31:33 +0900

    councilページ　細かい修正

---
+++ js/council.js
@@ -0,0 +1,11 @@
+$(window).scroll(function () {
+                $('.fadein').each(function () {
+                                var elemPos = $(this).offset().top,
+                                                scroll = $(window).scrollTop(),
+                                                windowHeight = $(window).height();
+
+                                if (scroll > elemPos - windowHeight + 150) {
+                                                $(this).addClass('scrollin');
+                                }
+                });
+});
